157afe55ec30ecd7334d1c804ae24abde43206a3
Ivan <ivandrelichman@gmail.com>
2019-01-09 17:43:22 -0300

    testing heroku version before branching

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7fw_n$ff8&!noiaxtei)uo3ky47#9-lh$a^=rlb5oew_b+fjpu'
+SECRET_KEY = os.environ.get("DJANGO_TEST_SK")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
